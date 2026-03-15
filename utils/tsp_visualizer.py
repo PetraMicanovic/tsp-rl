7648207ce514c7a41158da3874e1852ab9415e21
PetraMicanovic <micanovic.petra@yahoo.com>
2026-03-15 21:03:26 +0100

    Add comments to TSPVisualizer class

--- utils/tsp_visualizer.py
+++ utils/tsp_visualizer.py
@@ -1,17 +1,32 @@
 import matplotlib.pyplot as plt
 
 class TSPVisualizer:
+    """
+    Utility class used for visualizing TSP solutions.
+    """
     def __init__(self, points):
+        """
+        Parameters 
+        points: list of tuples
+            Coordinates of cities[(x1, y1), (x2, y2), ...]
+        """
         self.points = points
 
-    def plot_route(self, route, title = "TSP Solution")
-        
+    def plot_route(self, route, title = "TSP Solution"):
+        """
+        Plot the TSP route.
+
+        Parameters
+        route: list[int]
+            Order of visited cities
+        title: str
+        """
         x = [self.points[i][0] for i in route]
         y = [self.points[i][1] for i in route]
 
         plt.figure()
 
-        plt.plot(x,y, marker = "o")
+        plt.plot(x, y, marker = "o")
 
         for i, (px, py) in enumerate(self.points):
             plt.text(px, py, str(i))
