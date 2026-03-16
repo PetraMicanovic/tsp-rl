2825a9e56f1453f16dcc184bf33a877fbb6dae47
PetraMicanovic <micanovic.petra@yahoo.com>
2026-03-16 10:24:00 +0100

    Add comments to  TrainingPlotter class

--- utils/training_plotter.py
+++ utils/training_plotter.py
@@ -1,10 +1,36 @@
 import matplotlib.pyplot as plt 
 
 class TrainingPlotter:
+    """
+    The Training plotter class provides methods for:
+        - plotting reward per episode
+        - plotting moving average reward
+        - comparing multiple algorithms on a single graph
+    """
     def __init__(self, window = 100):
+        """
+        Initialize the plotter.
+
+        Parameters
+        window: int
+            Window size used for computing moving average smoothing.
+        """
         self.window =  window
     
     def moving_average(self, rewards):
+        """
+        Compute moving average of rewards.
+
+        Moving average is used to smooth noisy reinforcement learning reward curves so that learning trends become easier to observe.
+
+        Parameters
+        rewards: list[float]
+            Reward obtained in each episode.
+        
+        Returns
+        averages: list[float]
+            Smoothed reward values
+        """
         if len(rewards) < self.window:
             return rewards
         
@@ -16,9 +42,19 @@ class TrainingPlotter:
         return averages
     
     def plot_rewards(self, rewards, title = "Training rewards"):
+        """
+        Plot reward per episode together with the moving average curve.
+
+        Parameters
+        rewards: list[float]
+            Reward obtained in each episode
+        title: str
+            Title of the plot
+        """
         plt.figure()
         plt.plot(rewards, label = "Reward per episode")
         
+        #Smoothed curve
         smoothed = self.moving_average(rewards)
         plt.plot(range(len(smoothed)), smoothed, label = "Moving average")
         
@@ -29,6 +65,15 @@ class TrainingPlotter:
         plt.show()
 
     def compare_algorithms(self, results, title = "Algorithm Comparison"):
+        """
+        Plot comparison of multiple algorithms on a single graph.
+
+        Parameters
+        results: dict
+            Dictionary mapping algorithm name to reward list.
+        title: str
+            Title of the plot
+        """
         plt.figure()
 
         for algorithm_name, rewards in results.items():
