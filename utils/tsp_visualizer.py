9a0bea76df08de62e5e5432d4ea9d2082746fa7d
PetraMicanovic <micanovic.petra@yahoo.com>
2026-03-16 12:43:27 +0100

    Add route animation support to TSPVisualizer

--- utils/tsp_visualizer.py
+++ utils/tsp_visualizer.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import os
+import matplotlib.animation as animation
 
 class TSPVisualizer:
     """
@@ -50,4 +51,69 @@ class TSPVisualizer:
         save_path = os.path.join(save_dir, filename)
 
         plt.savefig(save_path)
+        plt.close()
+
+    def animate_route(self, route, algorithm_name, num_points):
+        """
+        Create and save an animation of the TSP route being constructed.
+
+        Parameters
+        route: list[int]
+            Order in which cities are visited
+        algorithm_name: str
+            Name of the RL algorithm
+        num_points: int
+            Number of cities in the environment
+        """
+
+        save_dir = os.path.join("results/animations", algorithm_name)
+        os.makedirs(save_dir, exist_ok = True)
+
+        fig, ax = plt.subplots()
+
+        x_points = [p[0] for p in self.points]
+        y_points = [p[1] for p in self.points]
+
+        ax.scatter(x_points, y_points)
+
+        ax.set_xlim(min(x_points) - 5, max(y_points) + 5)
+        ax.set_ylim(min(y_points) - 5, max(y_points) + 5)
+
+        for i, (px, py) in enumerate(self.points):
+            ax.text(px, py, str(i))
+
+        line, = ax.plot([],[], marker = "o")
+
+        ax.set_title(f"{algorithm_name} - TSP Animation ({num_points} points)")
+        ax.set_xlabel("X")
+        ax.set_ylabel("Y")
+
+        def update(frame):
+            partial_route = route[:frame + 1]
+
+            x = [self.points[i][0] for i in partial_route]
+            y = [self.points[i][1] for i in partial_route]
+
+            line.set_data(x, y)
+
+            ax.patches.clear()
+
+            # Draw arrows showing direction of travel
+            for i in range(len(partial_route) - 1):
+                start = self.points[partial_route[i]]
+                end = self.points[partial_route[i + 1]] 
+
+                dx = end[0] - start[0]
+                dy = end[1] - start[1]
+
+                ax.arrow(start[0], start[1], dx, dy, head_width = 1.5, lenght_includes_head = True, color = "red", alpha = 0.7)
+            return line,
+
+        ani = animation.FuncAnimation(fig, update, frames = len(route), interval = 500, blit = True)
+
+        filename = f"tsp_animation_{num_points}.gif"
+        save_path = os.path.join(save_dir, filename)
+
+        ani.save(save_path, writer = "pillow")
+
         plt.close()
